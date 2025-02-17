5e5f484c1ce05e6d624b62901dbc1439d4d3f3ad
ilya <berestovila31@gmail.com>
2025-02-17 15:13:08 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FormatCheckExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");Console.WriteLine("This is a test.");
+        }
+    }
+}
